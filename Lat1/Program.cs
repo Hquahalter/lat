a5f841af3cc0604872e7f5024bc231e912bd0dd0
ASUS ROG <ASUS ROG@HEAVENFURY>
2018-08-09 09:59:07 +0800

    initial commit

---
+++ Lat1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lat1
+{
+    class Program
+    {
+        public int findMax(int num1, int num2)
+        {
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            String userInput;
+            int num1, num2, ret;
+            Program p = new Program();
+            Console.WriteLine("Simple Program for find Max number \n\n");
+            Console.Write("Number 1 : ");
+            userInput = Console.ReadLine();
+            num1 = Convert.ToInt32(userInput);
+            Console.Write("Number 2 : ");
+            userInput = Console.ReadLine();
+            num2 = Convert.ToInt32(userInput);
+            ret = p.findMax(num1, num2);
+            Console.WriteLine("Max value is : '{0}' x", ret);
+            Console.ReadKey();
+            {
+        }
+    }
+}
